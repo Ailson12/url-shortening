50e615df2447a572b50bc29260724ea5bd031a92
ailson <robertofeitosa06@gmail.com>
2023-05-14 15:27:41 -0400

    validating input

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 import { MenuMobile } from './modules/menu-mobile.js'
 import { ShortenLink } from './modules/shorten-link.js'
 
-const menu = new MenuMobile({
-  anchor: '#main-header',
-  buttonOpen: '#mobile-button',
-  buttonClose: '#mobile-button-close',
-})
-menu.addEvent()
+window.onload = () => {
+  const menu = new MenuMobile({
+    anchor: '#main-header',
+    buttonOpen: '#mobile-button',
+    buttonClose: '#mobile-button-close',
+  })
+  menu.addEvent()
 
-const shorten = new ShortenLink()
-shorten.addEvent()
+  const shorten = new ShortenLink()
+  shorten.addEvent()
+}
--- js/modules/shorten-link.js
+++ js/modules/shorten-link.js
@@ -1,16 +1,19 @@
 import { getElement } from '../utils/selector.js'
 
 const KEY_STORAGE_LINK = 'shortened-links'
+const INVALID_LINK = 2
 
 export class ShortenLink {
   constructor() {
+    this.messageInput = getElement('.shorten-input-wrapper p')
+    this.inputWrapper = getElement('.shorten-input-wrapper')
     this.linksWrapper = getElement('.links-wrapper')
     this.input = getElement('#shorten-input')
     this.buttonControl = getElement('#shorten-btn')
   }
 
   get valueInput() {
-    return this.input.value
+    return this.input.value?.trim() ?? ''
   }
 
   set valueInput(value = '') {
@@ -26,29 +29,36 @@ export class ShortenLink {
     this.valueInput = ''
   }
 
-  startFetchApi() {
-    this.setParamsButton('Carregando...', true)
-  }
-
-  finallyFetchApi() {
-    this.setParamsButton('Encurtar!', false)
-  }
-
   setParamsButton(text, disabled) {
     this.buttonControl.innerText = text
-    this.buttonControl.setAttribute('disabled', disabled)
+    this.buttonControl.setAttribute('data-loading', disabled)
   }
 
   fetchApiShorten(url = '') {
-    this.startFetchApi()
+    this.setParamsButton('Carregando...', true)
     fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
       .then((response) => response.json())
       .then((data) => {
+        if (!data.ok) {
+          return Promise.reject(data)
+        }
         this.addLinkInStorage(data.result)
         this.listStoredLinks()
-        this.clearValueInput()
+        this.input.focus()
       })
-      .finally(() => this.finallyFetchApi())
+      .catch((error) => {
+        let message = 'Erro ao encurtar link'
+        if (error.error_code === INVALID_LINK) {
+          message = 'Link inválido'
+        }
+
+        console.error(error)
+        window.alert(message)
+      })
+      .finally(() => {
+        this.clearValueInput()
+        this.setParamsButton('Encurtar!', false)
+      })
   }
 
   addLinkInStorage(result = {}) {
@@ -84,10 +94,52 @@ export class ShortenLink {
     this.addEventCopyLink()
   }
 
+  validForm() {
+    const rules = [
+      {
+        message: 'Campo obrigatório',
+        isInvalid: () => this.valueInput.length <= 0,
+      },
+      {
+        message: 'Link já encurtado',
+        isInvalid: () =>
+          this.links.some((link) =>
+            Object.values(link).includes(this.valueInput)
+          ),
+      },
+    ]
+
+    let index = 0
+    let currentRule = null
+    while (index < rules.length) {
+      const rule = rules[index]
+      if (rule.isInvalid()) {
+        currentRule = rule
+        break
+      }
+
+      index++
+    }
+
+    const isValid = currentRule === null
+
+    if (isValid) {
+      this.inputWrapper.classList.remove('invalid')
+    } else {
+      this.input.focus()
+      this.messageInput.innerText = currentRule.message
+      this.inputWrapper.classList.add('invalid')
+    }
+
+    return isValid
+  }
+
   addEventButtonControl() {
-    this.buttonControl.addEventListener('click', () => {
-      this.fetchApiShorten(this.valueInput)
-    })
+    this.buttonControl.onclick = () => {
+      if (this.validForm()) {
+        this.fetchApiShorten(this.valueInput)
+      }
+    }
   }
 
   addEventCopyLink() {
